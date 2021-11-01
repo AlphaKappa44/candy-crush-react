3ea6d9d51c3eb3022b975bd50b2d0bdec3de1c79
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-11-01 22:44:11 +0100

    console.log create board, random colors, useState to keep the array and useEffect to use only once

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
 
-function App() {
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow',
+]
+
+const App = () => {
+  const[currentColorArrangement, setcurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomNumberFrom0To5 = Math.floor(Math.random() * candyColors.length)
+      const randomColor = candyColors[randomNumberFrom0To5]
+      randomColorArrangement.push(randomColor)
+    }
+    // console.log(randomColorArrangement)
+    setcurrentColorArrangement(randomColorArrangement)
+  }
+
+// createBoard() in a useEffect with empty array to be used only once 
+useEffect(() => {
+  createBoard()
+}, [])
+
+console.log(currentColorArrangement)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
     </div>
   );
 }
